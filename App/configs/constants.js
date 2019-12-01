f19f94adbb2629b072198a3de12f6612924fa5de
manish kumar <manish.kumar212111@gmail.com>
2019-12-01 18:43:08 +0530

    first commit

--- App/configs/constants.js
+++ App/configs/constants.js
@@ -1,7 +1,7 @@
-global.HOST_DB     = 'us-cdbr-iron-east-05.cleardb.net';
-global.USER_DB     = 'b7a9df3019d0d1';
-global.PASSWORD_DB = '95cdd49d';
-global.DATABASE_DB = 'heroku_bf9f8e6eec477f1';
+global.HOST_DB     = '43.255.154.41';
+global.USER_DB     = 'datakrop';
+global.PASSWORD_DB = 'Datakrop@2019';
+global.DATABASE_DB = 'rsrules';
 
 global.LIMIT_START_DATE = '2019-12-15';
 global.LIMIT_END_DATE = '2020-01-15';
